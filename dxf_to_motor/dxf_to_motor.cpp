a9a7bcc2eb7520370f4dbc6f742cb19fc0850a28
lanq21 <1037617364@qq.com>
2022-07-22 09:12:05 +0800

    Added the effect of polylines on rectangle extents

--- dxf_to_motor/dxf_to_motor.cpp
+++ dxf_to_motor/dxf_to_motor.cpp
@@ -80,14 +80,13 @@ void dxf_to_txt::addVertex(const DL_VertexData& data) {
 	static double x;
 	static double y;
 	static double bulge = 0;
-	static bool flag = false;
 	static bool first_called = true;
 	if (bulge)
 	{
 		double mu = (1 - bulge * bulge) / (2 * bulge);
 		double L = sqrt((data.x - x) * (data.x - x) + (data.y - y) * (data.y - y));
-		double r = L * (bulge + 1.0 / bulge) / 4.0;
-		if (r < 0) r = -r;
+		double radius = L * (bulge + 1.0 / bulge) / 4.0;
+		if (radius < 0) radius = -radius;
 		double cx = ((x + data.x) - (data.y - y) * mu) / 2.0;
 		double cy = ((y + data.y) + (data.x - x) * mu) / 2.0;
 		double angle1 = atan2(y - cy, x - cx);
@@ -96,19 +95,51 @@ void dxf_to_txt::addVertex(const DL_VertexData& data) {
 		double angle2 = atan2(data.y - cy, data.x - cx);
 		angle2 = angle2 / M_PI * 180;
 		if (angle2 < 0) angle2 = angle2 + 360;
-		new Arc_Polyline(cx, cy, r, angle1, angle2);
+
+		// 右侧边缘点
+		double Angle_1 = angle1;
+		double Angle_2 = angle2;
+		if (Angle_1 > Angle_2) max_x = std::max(max_x, cx + radius);
+		else max_x = std::max({ max_x,cx + radius * cos(angle1),cx + radius * cos(angle2) });
+
+		// 左侧边缘点
+		Angle_1 = (angle1 > 180) ? (angle1 - 180) : (angle1 + 180);
+		Angle_2 = (angle2 > 180) ? (angle2 - 180) : (angle2 + 180);
+		if (Angle_1 > Angle_2) min_x = std::min({ min_x,cx - radius });
+		else min_x = std::min({ min_x,cx + radius * cos(angle1),cx + radius * cos(angle2) });
+
+		// 上侧边缘点
+		Angle_1 = (angle1 > 90) ? (angle1 - 90) : (angle1 + 270);
+		Angle_2 = (angle2 > 90) ? (angle2 - 90) : (angle2 + 270);
+		if (Angle_1 > Angle_2) max_y = std::max({ max_y,cy + radius });
+		else max_y = std::max({ max_y,cy + radius * sin(angle1),cy + radius * sin(angle2) });
+
+		// 下侧边缘点
+		Angle_1 = (angle1 > 270) ? (angle1 - 270) : (angle1 + 90);
+		Angle_2 = (angle2 > 270) ? (angle2 - 270) : (angle2 + 90);
+		if (Angle_1 > Angle_2) min_y = std::min({ min_y,cy - radius });
+		else min_y = std::min({ min_y,cy + radius * sin(angle1),cy + radius * sin(angle2) });
+
+		new Arc_Polyline(cx, cy, radius, angle1, angle2);
 	}
 	else if (first_called)
 		first_called = false;
 	else
+	{
+		max_x = std::max({ max_x, x, data.x });
+		min_x = std::min({ min_x, x, data.x });
+		max_y = std::max({ max_y, y, data.y });
+		min_y = std::min({ min_y, y, data.y });
+		
 		new Line_Polyline(x, y, data.x, data.y);
+	}
 
 	x = data.x;
 	y = data.y;
 	bulge = data.bulge;
 
 	// 凸度!=0，则该点与下一点之间连有圆弧
-	// 圆弧半径 r=L*(W+1.0/W)/4.0，其中L为两点间距离
+	// 圆弧半径 radius=L*(W+1.0/W)/4.0，其中L为两点间距离
 	// 圆心	x=((x_1+x_2)-(y_2-y_1)*mu)/2.0
 	//		y=((y_1+y_2)+(x_2-x_1)*mu)/2.0
 	//		其中mu=(1-W*W)/2*W
